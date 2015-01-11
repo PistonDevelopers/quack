9c77ae0a2a1049da8acec3e6c8d876da36c41cc3
Sven Nilsen <bvssvni@gmail.com>
2015-01-11 14:41:28 +0100

    Redesign for new trait rules
    Closes https://github.com/PistonDevelopers/quack/issues/3

--- src/lib.rs
+++ src/lib.rs
@@ -8,54 +8,104 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use std::ops::{ Deref, DerefMut };
 
-impl<'a, T, U: GetFrom<T>> GetFrom<&'a RefCell<T>> for U {
+impl<'a, T, U> GetFrom for (U, &'a RefCell<T>)
+    where
+        (U, T): GetFrom<Property = U, Object = T>
+{
+    type Property = U;
+    type Object = &'a RefCell<T>;
+
     #[inline(always)]
-    fn get_from(obj: & &'a RefCell<T>) -> U {
-        GetFrom::get_from(obj.borrow().deref())
+    fn get_from(_: Me<Self>, obj: & &'a RefCell<T>) -> U {
+        GetFrom::get_from(Me::<(U, T)>, obj.borrow().deref())
     }
 }
 
-impl<T, U: GetFrom<T>> GetFrom<Rc<RefCell<T>>> for U {
+impl<T, U> GetFrom for (U, Rc<RefCell<T>>)
+    where
+        (U, T): GetFrom<Property = U, Object = T>
+{
+    type Property = U;
+    type Object = Rc<RefCell<T>>;
+
     #[inline(always)]
-    fn get_from(obj: &Rc<RefCell<T>>) -> U {
-        GetFrom::get_from(obj.borrow().deref())
+    fn get_from(_: Me<Self>, obj: &Rc<RefCell<T>>) -> U {
+        GetFrom::get_from(Me::<(U, T)>, obj.borrow().deref())
     }
 }
 
-impl<'a, F, T: SetAt<F>> SetAt<&'a RefCell<F>> for T {
+impl<'a, F, T> SetAt for (T, &'a RefCell<F>)
+    where
+        (T, F): SetAt<Property = T, Object = F>
+{
+    type Property = T;
+    type Object = &'a RefCell<F>;
+
     #[inline(always)]
-    fn set_at(self, obj: &mut &'a RefCell<F>) {
-        self.set_at(obj.borrow_mut().deref_mut())
+    fn set_at(_: Me<Self>, val: T, obj: &mut &'a RefCell<F>) {
+        SetAt::set_at(Me::<(T, F)>, val, obj.borrow_mut().deref_mut())
     }
 }
 
-impl<F, T: SetAt<F>> SetAt<Rc<RefCell<F>>> for T {
+impl<F, T> SetAt for (T, Rc<RefCell<F>>)
+    where
+        (T, F): SetAt<Property = T, Object = F>
+{
+    type Property = T;
+    type Object = Rc<RefCell<F>>;
+
     #[inline(always)]
-    fn set_at(self, obj: &mut Rc<RefCell<F>>) {
-        self.set_at(obj.borrow_mut().deref_mut())
+    fn set_at(_: Me<Self>, val: T, obj: &mut Rc<RefCell<F>>) {
+        SetAt::set_at(Me::<(T, F)>, val, obj.borrow_mut().deref_mut())
     }
 }
 
 
-impl<'a, F, T: ActOn<F, U>, U> ActOn<&'a RefCell<F>, U> for T {
+impl<'a, F, A, V> ActOn<V> for (A, &'a RefCell<F>)
+    where
+        (A, F): ActOn<V, Action = A, Object = F>
+{
+    type Action = A;
+    type Object = &'a RefCell<F>;
+
     #[inline(always)]
-    fn act_on(self, obj: &mut &'a RefCell<F>) -> U {
-        self.act_on(obj.borrow_mut().deref_mut())
+    fn act_on(_: Me<Self>, action: A, obj: &mut &'a RefCell<F>) -> V {
+        ActOn::act_on(Me::<(A, F)>, action, obj.borrow_mut().deref_mut())
     }
 }
 
-impl<F, T: ActOn<F, U>, U> ActOn<Rc<RefCell<F>>, U> for T {
+impl<F, A, V> ActOn<V> for (A, Rc<RefCell<F>>)
+    where
+        (A, F): ActOn<V, Action = A, Object = F>
+{
+    type Action = A;
+    type Object = Rc<RefCell<F>>;
+
     #[inline(always)]
-    fn act_on(self, obj: &mut Rc<RefCell<F>>) -> U {
-        self.act_on(obj.borrow_mut().deref_mut())
+    fn act_on(_: Me<Self>, action: A, obj: &mut Rc<RefCell<F>>) -> V {
+        ActOn::act_on(Me::<(A, F)>, action, obj.borrow_mut().deref_mut())
     }
 }
+
+/// Used to solve problems when calling static methods on traits.
+/// Calling static methods is necessary to solve implementation rules
+/// when reexporting property types.
+pub struct Me<T>;
 
 /// Something that can be set at an object.
+///
+/// Must be implemented on a `(Property, Object)`
 #[unstable]
-pub trait SetAt<F> {
+pub trait SetAt {
+    type Property;
+    type Object;
+
     /// Modify `F` with self.
-    fn set_at(self, &mut F);
+    fn set_at(
+        me: Me<Self>, 
+        val: <Self as SetAt>::Property,
+        obj: &mut <Self as SetAt>::Object
+    );
 }
 
 /// Automatically implemented through the `SetAt` trait.
@@ -68,25 +118,34 @@ pub trait Set<T> {
     fn set_mut(&mut self, val: T) -> &mut Self;
 }
 
-impl<T, U: SetAt<T>> Set<U> for T {
+impl<T, U> Set<U> for T
+    where
+        (U, T): SetAt<Property = U, Object = T>,
+{
     #[inline(always)]
     fn set(mut self, val: U) -> T {
-        val.set_at(&mut self);
+        SetAt::set_at(Me::<(U, T)>, val, &mut self);
         self
     }
 
     #[inline(always)]
     fn set_mut(&mut self, val: U) -> &mut T {
-        val.set_at(self);
+        SetAt::set_at(Me::<(U, T)>, val, self);
         self
     }
 }
 
 /// Something that can be retrieved from another object.
 #[unstable]
-pub trait GetFrom<T> {
+pub trait GetFrom {
+    type Property;
+    type Object;
+
     /// Gets value from object.
-    fn get_from(obj: &T) -> Self;
+    fn get_from(
+        me: Me<Self>, 
+        obj: &<Self as GetFrom>::Object
+    ) -> <Self as GetFrom>::Property;
 }
 
 /// Automatically implemented through the `GetFrom` trait.
@@ -96,31 +155,44 @@ pub trait Get<T> {
     fn get(&self) -> T;
 }
 
-impl<T, U: GetFrom<T>> Get<U> for T {
+impl<T, U> Get<U> for T
+    where
+        (U, T): GetFrom<Property = U, Object = T>
+{
     #[inline(always)]
     fn get(&self) -> U {
-        GetFrom::get_from(self)
+        GetFrom::get_from(Me::<(U, T)>, self)
     }
 }
 
 /// Does something to an object.
 #[unstable]
-pub trait ActOn<T, U> {
+pub trait ActOn<T> {
+    type Action;
+    type Object;
+
     /// Does something to an object.
-    fn act_on(self, &mut T) -> U;
+    fn act_on(
+        me: Me<Self>, 
+        action: <Self as ActOn<T>>::Action, 
+        obj: &mut <Self as ActOn<T>>::Object
+    ) -> T;
 }
 
 /// Automatically implemented through the `ActOn` trait.
 #[unstable]
-pub trait Action<T, U> {
+pub trait Action<A, V> {
     /// Does something.
-    fn action(&mut self, val: T) -> U;
+    fn action(&mut self, val: A) -> V;
 }
 
-impl<T, U: ActOn<T, V>, V> Action<U, V> for T {
+impl<T, A, V> Action<A, V> for T
+    where
+        (A, T): ActOn<V, Action = A, Object = T>
+{
     #[inline(always)]
-    fn action(&mut self, val: U) -> V {
-        val.act_on(self)
+    fn action(&mut self, action: A) -> V {
+        ActOn::act_on(Me::<(A, T)>, action, self)
     }
 }
 
